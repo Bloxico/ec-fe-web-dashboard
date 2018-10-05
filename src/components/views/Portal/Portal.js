1e4ed326752dc4b780cd0f8a1212a36b2c813a73
aleksaToljic <toljabre@gmail.com>
2018-10-05 20:19:14 +0200

    EC-43: start with button component

--- src/components/views/Portal/Portal.js
+++ src/components/views/Portal/Portal.js
@@ -8,7 +8,16 @@ const baseClass = `${THEME_PREFIX}-portal`;
 const Portal = () => (
   <div className={baseClass}>
     <h1>Portal page</h1>
-    <Button shape="round" color="white" data="aaa" tabIndex={-1} size="small" aria={{ 'pressed': true }} >Click Me!</Button>
+    <Button
+      shape="round"
+      color="white"
+      data="aaa"
+      tabIndex={-1}
+      size="small"
+      aria={{ pressed: true }}
+    >
+      Click Me!
+    </Button>
   </div>
 );
 
--- src/components/views/common/Button/Button.js
+++ src/components/views/common/Button/Button.js
@@ -6,20 +6,27 @@
 import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 import './Button.scss';
+
 // import Icon from '@components/Icon';
 // import svgBusy from '@images/icon-sync.svg';
 
 const baseClass = 'enrg-button';
 
 export type ButtonTypes =
-| 'primary'
-| 'secondary'
-| 'tertiary'
-| 'outline'
-| 'ghost'
-| 'link';
+  | 'primary'
+  | 'secondary'
+  | 'tertiary'
+  | 'outline'
+  | 'ghost'
+  | 'link';
 
-export type ButtonColors = 'success' | 'warning' | 'alert' | 'info' | 'inverse' | 'white';
+export type ButtonColors =
+  | 'success'
+  | 'warning'
+  | 'alert'
+  | 'info'
+  | 'inverse'
+  | 'white';
 
 export type ButtonShapes = 'round' | 'pill';
 
@@ -30,113 +37,113 @@ export type ButtonWidths = 'wide' | 'fixed' | 'full';
 export type ButtonActions = 'button' | 'submit' | 'reset';
 
 type PropsT = {
-    id?: string,
-    action?: ButtonActions,
-    busy?: boolean | null,
-    icon?: any,
-    disabled?: boolean,
-    hidden?: boolean,
-    pressed?: boolean,
-    tabIndex?: number,
-    type?: ButtonTypes,
-    color?: ButtonColors,
-    shape?: ButtonShapes,
-    size?: ButtonSizes,
-    width?: ButtonWidths,
-    value?: any,
-    inputRef?: any,
-    data: any,
-    aria: any,
-    children: any,
-    onClick?: Function,
-    className?: string,
+  id?: string,
+  action?: ButtonActions,
+  busy?: boolean | null,
+  icon?: any,
+  disabled?: boolean,
+  hidden?: boolean,
+  pressed?: boolean,
+  tabIndex?: number,
+  type?: ButtonTypes,
+  color?: ButtonColors,
+  shape?: ButtonShapes,
+  size?: ButtonSizes,
+  width?: ButtonWidths,
+  value?: any,
+  inputRef?: any,
+  data: any,
+  aria: any,
+  children: any,
+  onClick?: Function,
+  className?: string,
 };
 
 const createAttributes = type => data =>
-    Object.keys(data).reduce(
-        // $FlowIssue
-        (acc, attr) => ({ ...acc, [`${type}-${attr}`]: data[attr] }),
-        {},
-    );
+  Object.keys(data).reduce(
+    // $FlowIssue
+    (acc, attr) => ({ ...acc, [`${type}-${attr}`]: data[attr] }),
+    {},
+  );
 
 const dataAttributes = createAttributes('data');
 const ariaAttributes = createAttributes('aria');
 
 class Button extends PureComponent<PropsT> {
-    static defaultProps = {
-        data: {},
-        aria: {},
-    };
+  static defaultProps = {
+    data: {},
+    aria: {},
+  };
 
-    render() {
-        const {
-            id,
-            action = 'button',
-            busy,
-            disabled,
-            hidden,
-            pressed,
-            icon,
-            tabIndex,
-            type,
-            color,
-            shape,
-            size,
-            width,
-            value,
-            inputRef,
-            data,
-            aria,
-            onClick,
-            children,
+  render() {
+    const {
+      id,
+      action = 'button',
+      busy,
+      disabled,
+      hidden,
+      pressed,
+      icon,
+      tabIndex,
+      type,
+      color,
+      shape,
+      size,
+      width,
+      value,
+      inputRef,
+      data,
+      aria,
+      onClick,
+      children,
+      className,
+    } = this.props;
+
+    const classes =
+      type === 'link'
+        ? classNames(
+            'enrg-link',
+            icon && `enrg-link--icon`,
+            width && `enrg-link--${width}`,
+            size && `enrg-link--${size}`,
             className,
-        } = this.props;
+          )
+        : classNames(
+            baseClass,
+            icon && `${baseClass}--icon`,
+            type && `${baseClass}--${type}`,
+            color && `${baseClass}--${color}`,
+            shape && `${baseClass}--${shape}`,
+            width && `${baseClass}--${width}`,
+            size && `${baseClass}--${size}`,
+            className,
+          );
 
-        const classes =
-            type === 'link'
-                ? classNames(
-                'enrg-link',
-                icon && `enrg-link--icon`,
-                width && `enrg-link--${width}`,
-                size && `enrg-link--${size}`,
-                className,
-                )
-                : classNames(
-                baseClass,
-                icon && `${baseClass}--icon`,
-                type && `${baseClass}--${type}`,
-                color && `${baseClass}--${color}`,
-                shape && `${baseClass}--${shape}`,
-                width && `${baseClass}--${width}`,
-                size && `${baseClass}--${size}`,
-                className,
-                );
-
-        return (
-          <button
-            id={id}
-            type={action}
-            aria-busy={busy}
-            disabled={busy || disabled}
-            hidden={hidden}
-            aria-pressed={pressed}
-            tabIndex={tabIndex}
-            value={value}
-            ref={inputRef}
-            onClick={onClick}
-            className={classes}
-            {...dataAttributes(data)}
-            {...ariaAttributes(aria)}
-          >
-            {!busy &&
-                icon &&
-                typeof icon !== 'boolean' &&
-                typeof busy !== 'boolean'}
-            {typeof busy === 'boolean'}
-            {children}
-          </button>
-        );
-    }
+    return (
+      <button
+        id={id}
+        type={action}
+        aria-busy={busy}
+        disabled={busy || disabled}
+        hidden={hidden}
+        aria-pressed={pressed}
+        tabIndex={tabIndex}
+        value={value}
+        ref={inputRef}
+        onClick={onClick}
+        className={classes}
+        {...dataAttributes(data)}
+        {...ariaAttributes(aria)}
+      >
+        {!busy &&
+          icon &&
+          typeof icon !== 'boolean' &&
+          typeof busy !== 'boolean'}
+        {typeof busy === 'boolean'}
+        {children}
+      </button>
+    );
+  }
 }
 
 export default Button;
