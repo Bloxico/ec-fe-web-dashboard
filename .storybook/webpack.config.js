74a75147d3441bd5e3ee5700f5844deae4c9e7e5
Martin Stanojević <martin.stanojevic@gamecredits.com>
2018-10-06 23:57:22 +0200

    Add Button to Storybook.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,14 +5,68 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+const paths = require('../config/paths');
 
 module.exports = {
+  resolve: {
+    alias: {
+      src: paths.appSrc,
+      '@views': path.join(paths.appSrc, 'components/views'),
+      '@partials': path.join(paths.appSrc, 'components/views/common'),
+      '@ui': path.join(paths.appSrc, 'components/ui'),
+      '@styles': path.join(paths.appSrc, 'assets/styles'),
+      '@common-styles': path.join(paths.appSrc, 'assets/styles/main.scss'),
+      '@images': path.join(paths.appSrc, 'assets/images'),
+
+      // Support React Native Web
+      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      'react-native': 'react-native-web',
+    }
+  },
   plugins: [
     // your custom plugins
   ],
   module: {
     rules: [
       // add your custom rules.
+      {
+        test: /\.scss$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
 };
