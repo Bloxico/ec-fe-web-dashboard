9522ee6601c14d50f82ac9d6e0201c1322e680ef
Martin Stanojević <martin.stanojevic@gamecredits.com>
2018-10-10 10:39:14 +0200

    Format.

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,10 +1,4 @@
-
 import Form, { FormField, InputGroup } from './Form';
 import Textarea from './Textarea';
 
-export {
-  Form,
-  FormField,
-  InputGroup,
-  Textarea,
-};
+export { Form, FormField, InputGroup, Textarea };
