0cf49a935f6543dfd193d5445fa6689f2d214239
aleksaToljic <toljabre@gmail.com>
2018-10-10 15:10:46 +0200

    Revert "EC43"

--- src/components/views/Portal/Portal.js
+++ src/components/views/Portal/Portal.js
@@ -1,23 +1,12 @@
 import React from 'react';
 
 import { THEME_PREFIX } from 'src/constants/ui';
-import Button from '@partials/Button';
 
 const baseClass = `${THEME_PREFIX}-portal`;
 
 const Portal = () => (
   <div className={baseClass}>
     <h1>Portal page</h1>
-    <Button
-      shape="round"
-      color="white"
-      data="aaa"
-      tabIndex={-1}
-      size="small"
-      aria={{ pressed: true }}
-    >
-      Click Me!
-    </Button>
   </div>
 );
 
