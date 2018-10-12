e2bd61960c9bed4d279bad7c00771334718f1e4c
aleksaToljic <toljabre@gmail.com>
2018-10-12 23:18:47 +0200

    EC43: format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import configureStore from 'src/state/configureStore';
-import App from 'src/components/views/App';
 import 'src/assets/styles/main.scss';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import configureStore from 'src/state/configureStore';
+import App from 'src/components/views/App';
 
 const { store, history } = configureStore();
 
