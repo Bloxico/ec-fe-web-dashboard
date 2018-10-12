7c5199928400677fbb53684fc849eb3226814fdd
aleksaToljic <toljabre@gmail.com>
2018-10-12 23:12:39 +0200

    EC43: changed some colors for input,label and select updated Form and button story added checkbox styles from gc-lite remove box-shadow on %focus updated dev config for supporting css files

--- stories/Button.js
+++ stories/Button.js
@@ -8,15 +8,25 @@ import 'src/assets/styles/main.scss';
 
 storiesOf('Button', module)
     .add('Default', () => (
-      <Fragment>
-        <div style={{ width: '320px', margin: '50px auto' }}>
-          <Button
-            type="primary"
-            width="full"
-            onClick={action('clicked')}
-          >Primary button
-          </Button><span />
-          <Button onClick={action('clicked')} width="full" type="secondary">Secondary Button</Button>
-        </div>
-      </Fragment>
+        <Fragment>
+            <div style={{
+                backgroundColor: '#0c0f21',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%'
+            }}
+            >
+                <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
+                    <Button
+                      type="primary"
+                      width="full"
+                      onClick={action('clicked')}
+                    >Primary button
+                    </Button><span />
+                    <Button onClick={action('clicked')} width="full" type="secondary">Secondary Button</Button>
+                </div>
+            </div>
+        </Fragment>
     ));
--- stories/Form.js
+++ stories/Form.js
@@ -6,61 +6,84 @@ import { storiesOf } from '@storybook/react';
 import { Field } from 'redux-form';
 
 import Form, { FormField } from 'src/components/ui/Form';
-import Button from '@partials/Button';
 import 'src/assets/styles/main.scss';
 import withReduxForm from 'redux-form-storybook';
 
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import Label from "../src/components/ui/Label/Label";
+
+const citySelectItems = [
+    { value: 'SR', text: 'Serbia' },
+    { value: 'BG', text: 'Bulgaria' },
+{ value: 'RU', text: 'Russia' }
+];
 storiesOf('Form', module)
     .addDecorator(withReduxForm)
     .add('Form', () => (
             <Fragment>
                 <div style={{ backgroundColor: '#0c0f21', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
                     <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
-                        <h1>Create an account</h1>
                         <Form>
-                            {/* TODO@martins add validatiors */}
-                            <Field placeholder="Email" type="email" size="full" component={FormField} name="username" width="auto" />
+                            <Label text="Select multiple" disabled="true" />
+                            <Field placeholder="Select" multiple="true" type="select" options={citySelectItems} width="fixed" component={FormField} name="select" />
+                            <Label text="Select one" disabled="true" />
+                            <Field placeholder="Select" type="select" options={citySelectItems} width="fixed" component={FormField} name="select" />
+                            <Label text="Input default" disabled="true" />
                             <Field
                               size="full"
-                              type="password"
-                              placeholder="Password"
-                              component={FormField}
-                              name="password"
-                              width="auto"
-                            />
-                            <Field
-                              size="full"
-                              placeholder="Repeat Password"
-                              type="password"
-                              component={FormField}
-                              name="repeatPassword"
-                              width="auto"
-                            />
-                            <Field
-                              size="full"
-                              placeholder="Region"
                               type="text"
+                              placeholder="Text"
                               component={FormField}
-                              name="region"
-                              width="auto"
+                              name="text"
                             />
+                            <Label text="Input with success" disabled="true" />
                             <Field
                               size="full"
-                              placeholder="City"
                               type="text"
+                              placeholder="Text"
                               component={FormField}
-                              name="city"
-                              width="auto"
+                              name="text"
+                              status="success"
                             />
+                            <Label text="Input with error" disabled="true" />
                             <Field
                               size="full"
-                              placeholder="Nickname (Optional)"
                               type="text"
+                              placeholder="Text"
                               component={FormField}
-                              name="nickname"
-                              width="auto"
+                              name="text"
+                              status="error"
                             />
-                            <Button type="primary" size="full">Continue</Button>
+                            <Label text="Checkbox" disabled="true" />
+                            <Field
+                              type="checkbox"
+                              component={FormField}
+                              name="checkbox"
+                            />
+                            {/* <Field */}
+                              {/* size="full" */}
+                              {/* placeholder="Radio" */}
+                              {/* type="radio" */}
+                              {/* component={FormField} */}
+                              {/* name="radio" */}
+                            {/* /> */}
+
+                            {/* <Field */}
+                              {/* size="full" */}
+                              {/* placeholder="Switch" */}
+                              {/* type="switch" */}
+                              {/* component={FormField} */}
+                              {/* name="switch" */}
+                            {/* /> */}
+                            {/* <Field */}
+                              {/* size="full" */}
+                              {/* placeholder="Nickname (Optional)" */}
+                              {/* type="textarea" */}
+                              {/* component={FormField} */}
+                              {/* name="textarea" */}
+                            {/* /> */}
                         </Form>
                     </div>
                 </div>
