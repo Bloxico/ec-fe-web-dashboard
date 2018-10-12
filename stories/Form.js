344ed6cafe57b9cd60e59df6fd138d7ff009860c
aleksaToljic <toljabre@gmail.com>
2018-10-12 23:36:45 +0200

    EC43: format stories updated package.json to format stories as well

--- stories/Form.js
+++ stories/Form.js
@@ -12,81 +12,100 @@ import withReduxForm from 'redux-form-storybook';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import Label from "../src/components/ui/Label/Label";
+import Label from '../src/components/ui/Label/Label';
 
 const citySelectItems = [
-    { value: 'SR', text: 'Serbia' },
-    { value: 'BG', text: 'Bulgaria' },
-{ value: 'RU', text: 'Russia' }
+  { value: 'SR', text: 'Serbia' },
+  { value: 'BG', text: 'Bulgaria' },
+  { value: 'RU', text: 'Russia' },
 ];
 storiesOf('Form', module)
-    .addDecorator(withReduxForm)
-    .add('Form', () => (
-            <Fragment>
-                <div style={{ backgroundColor: '#0c0f21', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-                    <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
-                        <Form>
-                            <Label text="Select multiple" disabled="true" />
-                            <Field placeholder="Select" multiple="true" type="select" options={citySelectItems} width="fixed" component={FormField} name="select" />
-                            <Label text="Select one" disabled="true" />
-                            <Field placeholder="Select" type="select" options={citySelectItems} width="fixed" component={FormField} name="select" />
-                            <Label text="Input default" disabled="true" />
-                            <Field
-                              size="full"
-                              type="text"
-                              placeholder="Text"
-                              component={FormField}
-                              name="text"
-                            />
-                            <Label text="Input with success" disabled="true" />
-                            <Field
-                              size="full"
-                              type="text"
-                              placeholder="Text"
-                              component={FormField}
-                              name="text"
-                              status="success"
-                            />
-                            <Label text="Input with error" disabled="true" />
-                            <Field
-                              size="full"
-                              type="text"
-                              placeholder="Text"
-                              component={FormField}
-                              name="text"
-                              status="error"
-                            />
-                            <Label text="Checkbox" disabled="true" />
-                            <Field
-                              type="checkbox"
-                              component={FormField}
-                              name="checkbox"
-                            />
-                            {/* <Field */}
-                              {/* size="full" */}
-                              {/* placeholder="Radio" */}
-                              {/* type="radio" */}
-                              {/* component={FormField} */}
-                              {/* name="radio" */}
-                            {/* /> */}
+  .addDecorator(withReduxForm)
+  .add('Form', () => (
+    <Fragment>
+      <div
+        style={{
+          backgroundColor: '#0c0f21',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
+          <Form>
+            <Label text="Select multiple" disabled="true" />
+            <Field
+              placeholder="Select"
+              multiple="true"
+              type="select"
+              options={citySelectItems}
+              width="fixed"
+              component={FormField}
+              name="select"
+            />
+            <Label text="Select one" disabled="true" />
+            <Field
+              placeholder="Select"
+              type="select"
+              options={citySelectItems}
+              width="fixed"
+              component={FormField}
+              name="select"
+            />
+            <Label text="Input default" disabled="true" />
+            <Field
+              size="full"
+              type="text"
+              placeholder="Text"
+              component={FormField}
+              name="text"
+            />
+            <Label text="Input with success" disabled="true" />
+            <Field
+              size="full"
+              type="text"
+              placeholder="Text"
+              component={FormField}
+              name="text"
+              status="success"
+            />
+            <Label text="Input with error" disabled="true" />
+            <Field
+              size="full"
+              type="text"
+              placeholder="Text"
+              component={FormField}
+              name="text"
+              status="error"
+            />
+            <Label text="Checkbox" disabled="true" />
+            <Field type="checkbox" component={FormField} name="checkbox" />
+            {/* <Field */}
+            {/* size="full" */}
+            {/* placeholder="Radio" */}
+            {/* type="radio" */}
+            {/* component={FormField} */}
+            {/* name="radio" */}
+            {/* /> */}
 
-                            {/* <Field */}
-                              {/* size="full" */}
-                              {/* placeholder="Switch" */}
-                              {/* type="switch" */}
-                              {/* component={FormField} */}
-                              {/* name="switch" */}
-                            {/* /> */}
-                            {/* <Field */}
-                              {/* size="full" */}
-                              {/* placeholder="Nickname (Optional)" */}
-                              {/* type="textarea" */}
-                              {/* component={FormField} */}
-                              {/* name="textarea" */}
-                            {/* /> */}
-                        </Form>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    );
+            {/* <Field */}
+            {/* size="full" */}
+            {/* placeholder="Switch" */}
+            {/* type="switch" */}
+            {/* component={FormField} */}
+            {/* name="switch" */}
+            {/* /> */}
+            {/* <Field */}
+            {/* size="full" */}
+            {/* placeholder="Nickname (Optional)" */}
+            {/* type="textarea" */}
+            {/* component={FormField} */}
+            {/* name="textarea" */}
+            {/* /> */}
+          </Form>
+        </div>
+      </div>
+    </Fragment>
+  ));
