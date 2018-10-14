d9a92bfef0b3f9c07b0c3ae219ffb54a8499b419
Martin Stanojević <martin.stanojevic@gamecredits.com>
2018-10-15 00:24:26 +0200

    Add grid.

--- src/components/views/Auth/Login/Login.js
+++ src/components/views/Auth/Login/Login.js
@@ -1,9 +1,9 @@
 // @flow
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Field } from 'redux-form';
 
-import Form, { FormField } from '@ui/Form';
+import { Form, FormField, Container, Row, Col } from '@ui';
 
 import { THEME_PREFIX } from 'src/constants/ui';
 
@@ -16,25 +16,38 @@ export type Props = {
 const baseClass = `${THEME_PREFIX}-login`;
 
 const Login = ({ isLoginInProgress, handleLogin, handleSubmit }: Props) => (
-  <div className={baseClass}>
-    <h1>Login</h1>
+  <Fragment>
+    <Container>
+      <Row>
+        <Col>
+          <div className={baseClass}>
+            <h1>Login</h1>
 
-    <Form onSubmit={handleSubmit(handleLogin)}>
-      {/* TODO@martins add validatiors */}
-      <Field label="Email" component={FormField} name="username" width="auto" />
-      <Field
-        label="Password"
-        type="password"
-        component={FormField}
-        name="password"
-        width="auto"
-      />
+            <Form onSubmit={handleSubmit(handleLogin)}>
+              {/* TODO@martins add validatiors */}
+              <Field
+                label="Email"
+                component={FormField}
+                name="username"
+                width="auto"
+              />
+              <Field
+                label="Password"
+                type="password"
+                component={FormField}
+                name="password"
+                width="auto"
+              />
 
-      <button type="submit" disabled={isLoginInProgress}>
-        {isLoginInProgress ? 'Logging in' : 'Login'}
-      </button>
-    </Form>
-  </div>
+              <button type="submit" disabled={isLoginInProgress}>
+                {isLoginInProgress ? 'Logging in' : 'Login'}
+              </button>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </Fragment>
 );
 
 export default Login;
