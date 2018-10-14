0e0638970899c4d3de47699c0149103fd406e18b
Martin Stanojević <martin.stanojevic@gamecredits.com>
2018-10-14 23:37:15 +0200

    Add grid. Refactor storybook.

--- stories/Button.js
+++ stories/Button.js
@@ -1,31 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from '../src/components/ui/Button';
 
-storiesOf('Button', module).add('Default', () => (
-  <Fragment>
-    <div
-      style={{
-        backgroundColor: '#0c0f21',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
-        <Button type="primary" width="full" onClick={action('clicked')}>
-          Primary button
-        </Button>
-        <span />
-        <Button onClick={action('clicked')} width="full" type="secondary">
-          Secondary Button
-        </Button>
-      </div>
+storiesOf('Button', module).add('Button', () => (
+  <div className="panel panel--dark">
+    <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
+      <Button type="primary" width="full" onClick={action('clicked')}>
+        Primary button
+      </Button>
+      <span />
+      <Button onClick={action('clicked')} width="full" type="secondary">
+        Secondary Button
+      </Button>
     </div>
-  </Fragment>
+  </div>
 ));
--- stories/Form.js
+++ stories/Form.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { Field } from 'redux-form';
@@ -9,11 +9,6 @@ import Form, { FormField } from 'src/components/ui/Form';
 import 'src/assets/styles/main.scss';
 import withReduxForm from 'redux-form-storybook';
 
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import Label from '../src/components/ui/Label/Label';
-
 const citySelectItems = [
   { value: 'SR', text: 'Serbia' },
   { value: 'BG', text: 'Bulgaria' },
@@ -21,91 +16,61 @@ const citySelectItems = [
 ];
 storiesOf('Form', module)
   .addDecorator(withReduxForm)
-  .add('Form', () => (
-    <Fragment>
-      <div
-        style={{
-          backgroundColor: '#0c0f21',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <div style={{ width: '320px', margin: '50px auto', padding: '0 10px' }}>
-          <Form>
-            <Label text="Select multiple" disabled="true" />
-            <Field
-              placeholder="Select"
-              multiple="true"
-              type="select"
-              options={citySelectItems}
-              width="fixed"
-              component={FormField}
-              name="select"
-            />
-            <Label text="Select one" disabled="true" />
-            <Field
-              placeholder="Select"
-              type="select"
-              options={citySelectItems}
-              width="fixed"
-              component={FormField}
-              name="select"
-            />
-            <Label text="Input default" disabled="true" />
-            <Field
-              size="full"
-              type="text"
-              placeholder="Text"
-              component={FormField}
-              name="text"
-            />
-            <Label text="Input with success" disabled="true" />
-            <Field
-              size="full"
-              type="text"
-              placeholder="Text"
-              component={FormField}
-              name="text"
-              status="success"
-            />
-            <Label text="Input with error" disabled="true" />
-            <Field
-              size="full"
-              type="text"
-              placeholder="Text"
-              component={FormField}
-              name="text"
-              status="error"
-            />
-            <Label text="Checkbox" disabled="true" />
-            <Field type="checkbox" component={FormField} name="checkbox" />
-            {/* <Field */}
-            {/* size="full" */}
-            {/* placeholder="Radio" */}
-            {/* type="radio" */}
-            {/* component={FormField} */}
-            {/* name="radio" */}
-            {/* /> */}
-
-            {/* <Field */}
-            {/* size="full" */}
-            {/* placeholder="Switch" */}
-            {/* type="switch" */}
-            {/* component={FormField} */}
-            {/* name="switch" */}
-            {/* /> */}
-            {/* <Field */}
-            {/* size="full" */}
-            {/* placeholder="Nickname (Optional)" */}
-            {/* type="textarea" */}
-            {/* component={FormField} */}
-            {/* name="textarea" */}
-            {/* /> */}
-          </Form>
-        </div>
-      </div>
-    </Fragment>
+  .add('Input', () => (
+    <div className="panel panel--dark">
+      <Form>
+        <Field
+          label="Empty input"
+          component={FormField}
+          name="input1"
+          placeholder="placeholder"
+        />
+        <Field
+          label="Disabled"
+          component={FormField}
+          name="input1"
+          placeholder="Disabled placeholder"
+          disabled
+        />
+      </Form>
+    </div>
+  ))
+  .add('Select', () => (
+    <div className="panel panel--dark">
+      <Form>
+        <Field
+          placeholder="Select"
+          type="select"
+          options={citySelectItems}
+          width="fixed"
+          component={FormField}
+          name="select"
+        />
+        <Field
+          placeholder="Select"
+          type="select"
+          options={citySelectItems}
+          width="fixed"
+          component={FormField}
+          name="select"
+        />
+      </Form>
+    </div>
+  ))
+  .add('Checkbox', () => (
+    <div className="panel panel--dark">
+      <Form>
+        <Field
+          label="Checkbox"
+          type="checkbox"
+          component={FormField}
+        />
+          <Field
+            label="Disabled"
+            type="checkbox"
+            disabled
+            component={FormField}
+          />
+      </Form>
+    </div>
   ));
