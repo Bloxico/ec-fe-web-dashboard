844e7513c414f46d6472a7195b16ec595cc493e6
Martin Stanojević <martin.stanojevic@gamecredits.com>
2018-10-27 23:06:06 +0200

    Add chart component.

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,5 +2,16 @@ import Form, { FormField, InputGroup } from './Form';
 import Textarea from './Textarea';
 import Button from './Button';
 import { Container, Row, Col } from './Grid';
+import { LineChart } from './Chart';
 
-export { Button, Form, FormField, InputGroup, Textarea, Container, Row, Col };
+export {
+  Button,
+  Form,
+  FormField,
+  InputGroup,
+  Textarea,
+  Container,
+  Row,
+  Col,
+  LineChart,
+};
--- src/components/views/Portal/Dashboard/Dashboard.js
+++ src/components/views/Portal/Dashboard/Dashboard.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 
+import { LineChart } from '@ui';
+import { mockChartData } from 'src/mocks/dashboard';
+
 export type Props = {
   MSGDashboard: string,
 };
@@ -9,7 +12,12 @@ export type Props = {
 const baseClass = 'gc-dashboard';
 
 const Dashboard = ({ MSGDashboard }: Props) => (
-  <div className={baseClass}>{MSGDashboard}</div>
+  <div className={baseClass}>
+    {MSGDashboard}
+
+    <LineChart data={mockChartData(6)} colors={['green']} />
+    <LineChart data={mockChartData(6)} colors={['blue']} />
+  </div>
 );
 
 export default Dashboard;
