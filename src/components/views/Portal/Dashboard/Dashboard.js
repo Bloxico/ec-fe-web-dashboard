102c8431687f9092c3d5bda10b44f76484143c46
aleksaToljic <toljabre@gmail.com>
2018-11-20 15:49:36 +0100

    EC-116: fixed chart not showing summed value of certain date

--- src/components/views/Portal/Dashboard/Dashboard.js
+++ src/components/views/Portal/Dashboard/Dashboard.js
@@ -35,16 +35,22 @@ class Dashboard extends Component<Props> {
 
   render() {
     const { MSGDashboard, MSGCO2Prevented, transactions } = this.props;
-    const data = {};
+    let data = {};
     let balance = 0;
     let totalVirtualCurrency = 0;
     const date = new Date();
     date.setMonth(date.getMonth() - 3);
     if (transactions) {
-      transactions
+      data = transactions
         .filter(o => new Date(o.created) >= date)
-        .forEach(item => {
-          data[new Date(item.created).toLocaleDateString()] = item.enrgAmount;
+        .reduce((obj, item) => {
+          const newDate = new Date(item.created).toLocaleDateString();
+          if (newDate in obj) {
+            obj[newDate] += item.enrgAmount;
+          } else {
+            obj[newDate] = item.enrgAmount;
+          }
+          return obj;
         }, {});
       transactions.forEach(e => {
         balance += e.enrgAmount;
@@ -55,22 +61,22 @@ class Dashboard extends Component<Props> {
     return (
       <div className={baseClass}>
         <Header action="menu" title={MSGDashboard} />
-
-        <dl className={`${baseClass}__info`}>
-          <dt className={`${baseClass}__title`}>Total accumulated</dt>
-          <dd className={`${baseClass}__total`}>ENRG {balance.toFixed(2)}</dd>
-          <dd className={`${baseClass}__note`}>1 ENRG = x EUR</dd>
-        </dl>
-
-        <section className={`${baseClass}__group`}>
+        <div className={`${baseClass}__wrapper`}>
           <dl className={`${baseClass}__info`}>
-            <dt className={`${baseClass}__title`}>Bicycle Bits</dt>
-            <dd className={`${baseClass}__amount`}>
-              {totalVirtualCurrency.toFixed(2)}
-            </dd>
+            <dt className={`${baseClass}__title`}>Total accumulated</dt>
+            <dd className={`${baseClass}__total`}>ENRG {balance.toFixed(2)}</dd>
+            {/*/!*<dd className={`${baseClass}__note`}>1 ENRG = x EUR</dd>*!/ TODO@all get the value of ENRG*/}
           </dl>
-        </section>
 
+          <section className={`${baseClass}__group`}>
+            <dl className={`${baseClass}__info`}>
+              <dt className={`${baseClass}__title`}>Bicycle Bits</dt>
+              <dd className={`${baseClass}__amount`}>
+                {totalVirtualCurrency.toFixed(2)}
+              </dd>
+            </dl>
+          </section>
+        </div>
         <section className={`${baseClass}__chart`}>
           <Chart data={data} lineColor="#1be088" title={MSGCO2Prevented} />
         </section>
