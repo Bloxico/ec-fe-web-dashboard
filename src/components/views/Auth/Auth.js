2bf25e8df5e7958a4f580b7acb42e9f19c830da2
aleksaToljic <toljabre@gmail.com>
2018-11-27 15:32:43 +0100

    EC-136,EC-151,EC-157: router issues fixed

--- src/components/views/Auth/Auth.js
+++ src/components/views/Auth/Auth.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import {
   AUTH_PAGE,
@@ -11,7 +11,10 @@ import {
   VERIFY_PAGE,
   SUCCESS_PAGE,
   THEME_PREFIX,
+  AUTH_COOKIE,
+  PORTAL_PAGE,
 } from 'src/constants';
+import Cookie from 'src/services/cookie';
 
 import Login from './Login';
 import Register from './Register';
@@ -21,10 +24,14 @@ import Verify from './Verify';
 import Success from './Success';
 
 const baseClass = `${THEME_PREFIX}-auth`;
+const authCookie = Cookie.getJSON(AUTH_COOKIE);
 
 const Auth = () => (
   <div className={baseClass}>
     <Switch>
+      {authCookie && (
+        <Route path="*" component={() => <Redirect to={PORTAL_PAGE} />} />
+      )}
       <Route path={LOGIN_PAGE} component={Login} />
       <Route path={REGISTER_PAGE} component={Register} />
       <Route path={RESET_PASSWORD_PAGE} component={ResetPassword} />
