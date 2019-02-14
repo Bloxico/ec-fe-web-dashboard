ce41159762faf900e0a03ad7fc72828517caa35d
aleksaToljic <toljabre@gmail.com>
2019-02-14 13:47:13 +0100

    EC-221,EC-254,EC-255,EC-256

--- src/components/views/Auth/Welcome/Welcome.js
+++ src/components/views/Auth/Welcome/Welcome.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import classNames from 'classnames';
 
 import Logo from '@ui/Logo';
@@ -9,10 +9,13 @@ import {
   REGISTER_PAGE,
   LOGIN_PAGE,
   AUTH_PAGE,
+  DASHBOARD_PAGE,
   THEME_PREFIX,
+  AUTH_COOKIE,
   isProd,
 } from 'src/constants';
 import LanguageChange from '@partials/LanguageChange';
+import Cookie from 'src/services/cookie';
 
 // TODO@all fix this logo like it is in gc-lite, and links for term of use and privacy policy should open a modal or redirect to external link
 export type Props = {
@@ -44,7 +47,13 @@ class Welcome extends Component<Props> {
     super(props);
     const { location } = props;
     const catchUserId = new URLSearchParams(location.search).get('userId');
+    const catchToken = new URLSearchParams(location.search).get('token');
     this.externalId = isProd ? catchUserId : catchUserId || randomExternalId;
+
+    if (catchToken)
+      Cookie.setJSON(AUTH_COOKIE, {
+        accessToken: catchToken,
+      });
   }
 
   componentDidMount() {
@@ -63,6 +72,8 @@ class Welcome extends Component<Props> {
       MSGPrivacyPolicy,
       intl,
     } = this.props;
+
+    if (Cookie.getJSON(AUTH_COOKIE)) return <Redirect to={DASHBOARD_PAGE} />;
 
     return (
       <div className={baseClass}>
